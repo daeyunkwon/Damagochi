9f2fe4c68651a1d748a3c329cd62e3d8ceea0a4d
Daeyun <sweet_ray@naver.com>
2024-06-07 22:16:14 +0900

    Initial Commit

---
+++ Damagochi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Damagochi
+//
+//  Created by 권대윤 on 6/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
