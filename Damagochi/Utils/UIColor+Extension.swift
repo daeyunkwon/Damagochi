046a5f8a556aed2ac0d54117ed72ce212c8f981c
Daeyun <sweet_ray@naver.com>
2024-06-07 22:45:40 +0900

    Feat: UIColor와 UIFont에 Extension으로 사용할 색상 및 폰트 사이즈 정의

---
+++ Damagochi/Utils/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  Damagochi
+//
+//  Created by 권대윤 on 6/7/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customBackgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    
+    static let customFontColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+}
---
+++ Damagochi/Utils/UIFont+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIFont+Extension.swift
+//  Damagochi
+//
+//  Created by 권대윤 on 6/7/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let maximumSize = UIFont.systemFont(ofSize: 15)
+    
+    static let middleSize = UIFont.systemFont(ofSize: 14)
+    
+    static let minimumSize = UIFont.systemFont(ofSize: 13)
+}
